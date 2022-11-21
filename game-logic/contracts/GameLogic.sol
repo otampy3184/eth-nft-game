d74e2a5949efe917e15396f13c30d8d4464e6378
django_freeman <o.tampodo.t.18@gmail.com>
2022-11-21 22:37:08 +0900

    初期値設定

--- game-logic/contracts/GameLogic.sol
+++ game-logic/contracts/GameLogic.sol
@@ -4,7 +4,45 @@ pragma solidity ^0.8.9;
 import "../node_modules/hardhat/console.sol";
 
 contract GameLogic {
-    constructor(){
-        console.log("constructed.");
+    // Store charactor data
+    struct CharacterAttributes {
+        uint256 characterIndex;
+        string name;
+        string imageURI;
+        uint256 hp;
+        uint256 maxHp;
+        uint256 attackDamage;
+    }
+    CharacterAttributes[] defaultCharacters;
+
+    constructor(
+        string[] memory characterNames,
+        string[] memory characterImageURIs,
+        uint256[] memory characterHp,
+        uint256[] memory characterAttackDmg
+    ) {
+        // ゲームで扱う全てのキャラクターをループ処理で呼び出し、それぞれのキャラクターに付与されるデフォルト値をコントラクトに保存します。
+        for (uint256 i = 0; i < characterNames.length; i += 1) {
+            defaultCharacters.push(
+                CharacterAttributes({
+                    characterIndex: i,
+                    name: characterNames[i],
+                    imageURI: characterImageURIs[i],
+                    hp: characterHp[i],
+                    maxHp: characterHp[i],
+                    attackDamage: characterAttackDmg[i]
+                })
+            );
+            CharacterAttributes memory character = defaultCharacters[i];
+
+            // hardhat の console.log() では、任意の順番で最大4つのパラメータを指定できる。
+            // 使用できるパラメータの種類: uint, string, bool, address
+            console.log(
+                "Done initializing %s w/ HP %s, img %s",
+                character.name,
+                character.hp,
+                character.imageURI
+            );
+        }
     }
 }
