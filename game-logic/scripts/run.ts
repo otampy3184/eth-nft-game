34e181a3a878acd354d1252106ff31d8b3f69be5
django_freeman <o.tampodo.t.18@gmail.com>
2022-11-21 22:37:15 +0900

    テスト

--- game-logic/scripts/run.ts
+++ game-logic/scripts/run.ts
@@ -1,18 +1,27 @@
 import hre from "hardhat"
 
-const main = async() => {
+const main = async () => {
     const gameLogicContractFactory = await hre.ethers.getContractFactory("GameLogic")
-    const gameLogicContract = await gameLogicContractFactory.deploy()
+    const gameLogicContract = await gameLogicContractFactory.deploy(
+        ["ZORO", "NAMI", "USOPP"], // キャラクターの名前
+        [
+            "https://i.imgur.com/TZEhCTX.png", // キャラクターの画像
+            "https://i.imgur.com/WVAaMPA.png",
+            "https://i.imgur.com/pCMZeiM.png",
+        ],
+        [100, 200, 300], // キャラクターのHP
+        [100, 50, 25] // キャラクターの攻撃力
+    )
     await gameLogicContract.deployed()
 
     console.log("contract deployed at:", gameLogicContract.address)
 }
 
-const runMain = async() => {
+const runMain = async () => {
     try {
         await main()
         process.exit(0)
-    } catch(error) {
+    } catch (error) {
         console.log(error)
         process.exit(1)
     }
