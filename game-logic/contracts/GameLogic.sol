ee40ecf364abfaf2eec998e2221e1093b90fafa6
django_freeman <o.tampodo.t.18@gmail.com>
2022-11-22 22:07:35 +0900

    update contract

--- game-logic/contracts/GameLogic.sol
+++ game-logic/contracts/GameLogic.sol
@@ -1,9 +1,14 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "../node_modules/@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Counters.sol";
+import "../node_modules/@openzeppelin/contracts/utils/Strings.sol";
 import "../node_modules/hardhat/console.sol";
 
-contract GameLogic {
+contract GameLogic is ERC721 {
+    using Counters for Counters.Counter;
+    Counters.Counter private _tokenIds;
     // Store charactor data
     struct CharacterAttributes {
         uint256 characterIndex;
@@ -15,13 +20,16 @@ contract GameLogic {
     }
     CharacterAttributes[] defaultCharacters;
 
+    mapping(uint256 => CharacterAttributes) public nftHoldersAttributes;
+    mapping(address => uint256) public nftHolders;
+
     constructor(
         string[] memory characterNames,
         string[] memory characterImageURIs,
         uint256[] memory characterHp,
         uint256[] memory characterAttackDmg
-    ) {
-        // ゲームで扱う全てのキャラクターをループ処理で呼び出し、それぞれのキャラクターに付与されるデフォルト値をコントラクトに保存します。
+    ) ERC721("NFTGame", "ONFTG") {
+        // ゲームで扱う全てのキャラクターをループ処理で呼び出し、それぞれのキャラクターに付与されるデフォルト値をコントラクトに保存
         for (uint256 i = 0; i < characterNames.length; i += 1) {
             defaultCharacters.push(
                 CharacterAttributes({
@@ -35,7 +43,7 @@ contract GameLogic {
             );
             CharacterAttributes memory character = defaultCharacters[i];
 
-            // hardhat の console.log() では、任意の順番で最大4つのパラメータを指定できる。
+            // hardhat の console.log() では、任意の順番で最大4つのパラメータを指定できる
             // 使用できるパラメータの種類: uint, string, bool, address
             console.log(
                 "Done initializing %s w/ HP %s, img %s",
@@ -44,5 +52,27 @@ contract GameLogic {
                 character.imageURI
             );
         }
+        _tokenIds.increment();
+    }
+
+    function mintCharacterNFT(uint256 _characterIndex) external {
+        uint256 newItemId = _tokenIds.current();
+
+        _safeMint(msg.sender, newItemId);
+
+        nftHoldersAttributes[newItemId] = CharacterAttributes({
+            characterIndex: _characterIndex,
+            name: defaultCharacters[_characterIndex].name,
+            imageURI: defaultCharacters[_characterIndex].imageURI,
+            hp: defaultCharacters[_characterIndex].hp,
+            maxHp: defaultCharacters[_characterIndex].maxHp,
+            attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
+
+        console.log("Minted NFT /w tokenId %s and characterIndex %s", newItemId, _characterIndex);
+
+        nftHolders[msg.sender] = newItemId;
+
+        _tokenIds.increment();
     }
 }
